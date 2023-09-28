0afab022e9f6de84e6a46be1ff0fd70a572215ea
Heyward Fann <fannheyward@gmail.com>
2023-09-28 15:31:48 +0800

    chore: fmt

--- generate-config-schema.js
+++ generate-config-schema.js
@@ -1,24 +1,22 @@
-'use strict';
-
 // via https://github.com/DavidAnson/vscode-markdownlint/blob/master/generate-config-schema.js
 
-const fs = require('fs');
-const packageJsonPath = './package.json';
+const fs = require("fs");
+const packageJsonPath = "./package.json";
 const packageJson = require(packageJsonPath);
-const configurationSchema = require('./node_modules/markdownlint/schema/markdownlint-config-schema.json');
+const configurationSchema = require("./node_modules/markdownlint/schema/markdownlint-config-schema.json");
 const properties = configurationSchema.properties;
 for (const k of Object.keys(properties)) {
   const v = properties[k];
-  if (v['$ref']) {
-    const kk = v['$ref'].split('/').pop();
+  if (v.$ref) {
+    const kk = v.$ref.split("/").pop();
     properties[k] = properties[kk];
   }
 }
 configurationSchema.properties = properties;
 
 // Update package.json
-const configurationRoot = packageJson.contributes.configuration.properties['markdownlint.config'];
+const configurationRoot = packageJson.contributes.configuration.properties["markdownlint.config"];
 configurationRoot.properties = configurationSchema.properties;
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n');
+fs.writeFileSync(packageJsonPath, `${JSON.stringify(packageJson, null, 2)}\n`);
 
-fs.writeFileSync('./schemas/markdownlint-config-schema.json', JSON.stringify(configurationSchema, null, 2) + '\n');
+fs.writeFileSync("./schemas/markdownlint-config-schema.json", `${JSON.stringify(configurationSchema, null, 2)}\n`);
